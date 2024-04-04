f4a0fc9e82203dd939b084cb44a09767bacb3285
zen_vov <zholamanbv@gmail.com>
2024-04-04 23:42:40 +0500

    ad,

--- src/widgets/Layout/Header/ui/Header.tsx
+++ src/widgets/Layout/Header/ui/Header.tsx
@@ -5,11 +5,15 @@ import Button from "@/shared/ui/Button/Button";
 
 export default function Header() {
   return (
-    <header>
+    <header className="py-[30px]">
       <div className="container">
         <nav className="flex justify-between">
           <Logo />
-          <Input className="" placeholder="Поиск квартиры" />
+          <Input
+            className="focus:border-none active:border-none placeholder-border-black"
+            placeholder="Поиск квартиры"
+          />
+          <p className="text-red">dafs</p>
           <div className="flex gap-[40px]">
             <Button className="text-md font-[500]" label="Квартиры " />
             <Button className="text-md font-[500]" label="Подселение" />
