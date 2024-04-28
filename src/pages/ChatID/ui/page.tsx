97fa73b53f7b480826d145feda44342526a577d9
zen_vov <zholamanbv@gmail.com>
2024-04-28 22:12:29 +0500

    chat not completed

--- src/pages/ChatID/ui/page.tsx
+++ src/pages/ChatID/ui/page.tsx
@@ -5,14 +5,13 @@ import Arrow from "@/shared/ui/Icons/Arrow/Arrow";
 import axios from "axios";
 import Image from "next/image";
 import Link from "next/link";
-import { useParams } from "next/navigation";
 
 export default function ChatIdPage() {
-  const [chats, setChats] = useState([]);
-  const params = useParams() as { id: string | number };
+  const [data, setData] = useState([]);
+  const [authorName, setAuthorName] = useState("");
 
   useEffect(() => {
-    const fetchChats = async () => {
+    const fetchChat = async () => {
       try {
         const token = localStorage.getItem("accessToken");
         const res = await axios.get(`${BASE_URL}/chat/chats/`, {
@@ -20,40 +19,33 @@ export default function ChatIdPage() {
             Authorization: `JWT ${token}`,
           },
         });
-
-        const chatWithLastMessages = await Promise.all(
-          res.data.map(async (chat: any) => {
-            try {
-              const lastMessageRes = await axios.get(
-                `${BASE_URL}/chat/chats/${params.id}/messages/${chat.id}`,
-                {
-                  headers: {
-                    Authorization: `JWT ${token}`,
-                  },
-                }
-              );
-
-              return {
-                ...chat,
-                last_message: lastMessageRes.data.text,
-              };
-            } catch (error) {
-              console.error("Error fetching last message:", error);
-              return {
-                ...chat,
-                last_message: null,
-              };
-            }
-          })
-        );
-
-        setChats(chatWithLastMessages);
+        setData(res.data);
       } catch (err) {
         console.log(err);
       }
     };
 
-    fetchChats();
+    const userAuthor = async () => {
+      const accessToken = localStorage.getItem("accessItem");
+      const jwt = require("jsonwebtoken");
+
+      const decodedToken = jwt.decode(accessToken);
+      const userId = decodedToken?.user_id;
+
+      try {
+        const userRes = await fetch(
+          `http://studhouse.kz/api/v1/auth/user/${userId}/`
+        );
+        const user = await userRes.json();
+        setAuthorName(user.full_name);
+        console.log("user name for chat", user.full_name);
+      } catch (error) {
+        console.error("Error fetching user: ", error);
+      }
+    };
+
+    fetchChat();
+    userAuthor();
   }, []);
 
   return (
@@ -64,13 +56,13 @@ export default function ChatIdPage() {
       </Link>
       <h1 className="text-lg font-semibold mb-12">Все сообщения</h1>
       <div className="flex flex-col gap-10">
-        {chats.length === 0 ? (
+        {data.length === 0 ? (
           <p className="text-center text-lg font-medium">У вас нет сообщений</p>
         ) : (
-          chats.map(({ id, text }) => (
+          data.map(({ id, creationDate, interlocutor }) => (
             <Link href={`/routs/chat/${id}`} className="flex gap-9" key={id}>
               <Image
-                src={""}
+                src={"/"}
                 className="rounded-[12px]"
                 width={247}
                 height={123}
@@ -80,9 +72,13 @@ export default function ChatIdPage() {
                 <h1 className="text-lg font-medium white-space">
                   г. Алматы, Бостандыкский район · 3-х комнатная квартира
                 </h1>
-                <p className="text-md font-light">
-                  {text ? text : "Нет сообщений"}
-                </p>
+                <div className="flex items-center gap-[16px]">
+                  {authorName}:
+                  <p className="text-md font-light">
+                    Здравствуйте! Хотели бы снять эту квартиру
+                  </p>
+                </div>
+                <p>{creationDate}</p>
               </div>
             </Link>
           ))
