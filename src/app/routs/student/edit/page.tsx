12b91f7946a602a90901b841978a86e305e67fb2
Bagdat <clonn262@gmail.com>
2024-05-03 21:44:29 +0500

    pre-commit fixed

---
+++ src/app/routs/student/edit/page.tsx
@@ -0,0 +1,11 @@
+import Layout from "@/widgets/Layout/Layout";
+
+export default function Home() {
+  return (
+    <Layout>
+      <main className="container">
+        
+      </main>
+    </Layout>
+  )
+}
---
+++ src/pages/EditRelocation/ui/EditRelocation.tsx
@@ -0,0 +1,7 @@
+export default function EditRelocation() {
+  return (
+    <section>
+      
+    </section>
+  )
+}
