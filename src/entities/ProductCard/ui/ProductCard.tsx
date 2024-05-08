9cd1029aee5a256b9f06146540c3c189888cd4eb
zen_vov <zholamanbv@gmail.com>
2024-05-08 15:35:22 +0500

    fix

--- src/entities/ProductCard/ui/ProductCard.tsx
+++ src/entities/ProductCard/ui/ProductCard.tsx
@@ -18,6 +18,7 @@ export type ProductProps = {
   price: string;
   advertisement_images: { image: string | any | null }[];
   creationDate: string;
+  userRole: "Student" | "Landlord" | null;
 };
 
 export default function ProductCard(props: ProductProps) {
@@ -29,6 +30,7 @@ export default function ProductCard(props: ProductProps) {
     advertisement_images,
     location,
     creationDate,
+    userRole,
   } = props;
   const [like, setLike] = useState<boolean>(false);
   const router = useRouter();
@@ -47,9 +49,33 @@ export default function ProductCard(props: ProductProps) {
       });
   };
 
-  const addToFavorite = () => {
-    setLike(!like);
-    alert("Добавлено в избранные!");
+  const addToFavorites = async () => {
+    try {
+      const accessToken = localStorage.getItem("accessToken");
+      if (!accessToken) {
+        console.error("Access token not found.");
+        return;
+      }
+      const res = await axios.post(
+        `${BASE_URL}/advertisement/add_to_favorite/`,
+        {
+          advertisement: id,
+        },
+        {
+          headers: {
+            Authorization: `JWT ${accessToken}`,
+          },
+        }
+      );
+      console.log("Added to favorites:", res.data);
+      setIsFavorite(true);
+    } catch (error) {
+      console.error("Error adding to favorites:", error);
+    }
+  };
+
+  const navigateToProduct = () => {
+    router.push(`/routs/product/${id}`);
   };
 
   return (
@@ -76,9 +102,22 @@ export default function ProductCard(props: ProductProps) {
           <h5 className="text-sm">Опубликовано в {creationDate}</h5>
         </div>
         <div className="flex items-start justify-center gap-[1rem]">
-          <button onClick={() => addToFavorite()}>
-            <Like className={like ? "like" : ""} />
-          </button>
+          {userRole == "Student" && (
+            <svg
+              className="cursor-pointer"
+              onClick={addToFavorites}
+              xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="20"
+              viewBox="0 0 20 20"
+              fill="none"
+            >
+              <path
+                d="M10.0002 3.66405L9.1039 2.68139C7.00008 0.37473 3.14246 1.17073 1.74992 4.07072C1.09615 5.43471 0.948648 7.40404 2.14243 9.91737C3.29247 12.3374 5.68504 15.236 10.0002 18.3933C14.3153 15.236 16.7066 12.3374 17.8579 9.91737C19.0517 7.40271 18.9054 5.43471 18.2504 4.07072C16.8579 1.17073 13.0003 0.373397 10.8965 2.68006L10.0002 3.66405ZM10.0002 20C-9.16666 6.49071 4.09874 -4.05326 9.78017 1.52406C9.85517 1.59784 9.9285 1.67384 10.0002 1.75206C10.0706 1.67334 10.144 1.59773 10.2202 1.52539C15.9004 -4.05592 29.167 6.48938 10.0002 20Z"
+                fill={`${isFavorite ? "red" : "black"}`}
+              />
+            </svg>
+          )}
           <button onClick={copyLinkToClipboard}>
             <Share />
           </button>
--- src/widgets/productList/ui/productLIst.tsx
+++ src/widgets/productList/ui/productLIst.tsx
@@ -3,25 +3,57 @@ import ProductCard, {
   ProductProps,
 } from "@/entities/ProductCard/ui/ProductCard";
 import Link from "next/link";
+import React from "react";
 
 interface ProductListI {
   records?: ProductProps[];
 }
 
 export default function ProductList({ records }: ProductListI) {
+  const [userRole, setUserRole] = React.useState<"Student" | "Landlord" | null>(
+    null
+  );
+
+  React.useEffect(() => {
+    const accessToken = localStorage.getItem("accessToken");
+    const jwt = require("jsonwebtoken");
+
+    const decodedToken = jwt.decode(accessToken);
+    const userId = decodedToken?.user_id;
+
+    const fetchRole = async () => {
+      try {
+        const userResponse = await fetch(
+          `http://studhouse.kz/api/v1/auth/user/${userId}/`
+        );
+        const user = await userResponse.json();
+        console.log("user role: ", user.role.role_name);
+        setUserRole(user.role.role_name);
+      } catch (error) {
+        console.error("Error fetching user role: ", error);
+      }
+    };
+
+    fetchRole();
+  }, []);
+
   return (
     <>
       {records?.map(
         ({ id, location, price, creationDate, advertisement_images }) => (
-          <Link href={`/routs/product/${id}`} key={id} passHref>
+          // <Link href={`/routs/product/${id}`} key={id} passHref>
+          <div>
             <ProductCard
               id={id}
               location={location}
+              userRole={userRole}
               price={price}
               creationDate={creationDate}
               advertisement_images={advertisement_images}
             />
-          </Link>
+          </div>
+
+          // </Link>
         )
       )}
     </>
