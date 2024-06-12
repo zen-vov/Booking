f05019481dd1eb81352bccea3a5272d825dee6c1
nursen <200103098@stu.sdu.edu.kz>
2024-06-12 21:13:04 +0500

    changed Modal.tsx

--- src/shared/ui/Modal/ui/Modal.tsx
+++ src/shared/ui/Modal/ui/Modal.tsx
@@ -2,7 +2,7 @@ import React, { useRef, useEffect } from "react";
 import cn from "classnames";
 
 interface ModalProps {
-  isOpen?: any;
+  isOpen?: any
   onClose: () => void;
   children: React.ReactNode;
 }
@@ -11,11 +11,7 @@ const Modal: React.FC<ModalProps> = ({ onClose, children, isOpen }) => {
   const modalRef = useRef<HTMLDivElement>(null);
 
   const handleClickOutside = (event: MouseEvent) => {
-    if (
-      isOpen &&
-      modalRef.current &&
-      !modalRef.current.contains(event.target as Node)
-    ) {
+    if (modalRef.current && !modalRef.current.contains(event.target as Node)) {
       onClose();
     }
   };
@@ -25,24 +21,20 @@ const Modal: React.FC<ModalProps> = ({ onClose, children, isOpen }) => {
     return () => {
       document.removeEventListener("click", handleClickOutside);
     };
-  }, [isOpen]);
+  }, []);
 
   return (
-    <>
-      {isOpen && (
-        <div className="fixed z-[1000] inset-0 flex items-center justify-center bg-black bg-opacity-50">
-          <div
-            onClick={isOpen}
-            className="bg-white p-[64px] rounded-[15px] h-fit"
-            ref={modalRef}
-          >
-            <div className="flex flex-col justify-between py-[20px]">
-              {children}
-            </div>
-          </div>
+    <div className="fixed z-[1000] inset-0 flex items-center justify-center bg-black bg-opacity-50">
+      <div
+        onClick={isOpen}
+        className="bg-white p-[64px] rounded-[15px] h-fit"
+        ref={modalRef}
+      >
+        <div className="flex flex-col justify-between py-[20px]">
+          {children}
         </div>
-      )}
-    </>
+      </div>
+    </div>
   );
 };
 
